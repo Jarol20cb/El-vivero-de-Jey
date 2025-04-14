e2e429f5de5daff90182ec8e02ab02228b93dffa
JeySmith19 <jarol140202cb@gmail.com>
2025-04-14 14:32:40 -0500

    se mejoro el footer, se agrego img de busqueda no encontrada

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  constructor(private router: Router) {}
 
+  scrollToSection(section: string): void {
+    if (this.router.url !== '/') {
+      this.router.navigate(['/']).then(() => {
+        setTimeout(() => this.doScroll(section), 100);
+      });
+    } else {
+      this.doScroll(section);
+    }
+  }
+
+  doScroll(section: string) {
+    const element = document.getElementById(section);
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop - 50,
+        behavior: 'smooth'
+      });
+    }
+  }
 }
